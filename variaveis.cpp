df2e18c52b0aa48d86f830f079daa71a9adfe905
VitorNoe <162385647+VitorNoe@users.noreply.github.com>
2024-03-22 00:50:02 +0000

    aula sobre operadores logicos

---
+++ variaveis.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main() {
+
+    int numero =10;
+    float pi = 3.14159;
+    char letra =  'A';
+    std::string nome = "Ana";
+    bool isTrue = true;
+
+    printf("numero: %d\n", numero);
+    printf("pi: %.2f\n", pi);
+    printf("letra: %c\n", letra);
+    printf("nome: %s\n", nome.c_str());
+    printf("isTrue: %d\n", isTrue);
+
+    return 0;
+
+}
