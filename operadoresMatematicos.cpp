43186f8286147116bd272d5dd92d0c3e090d85f1
VitorNoe <162385647+VitorNoe@users.noreply.github.com>
2024-03-26 00:46:25 +0000

    Operadores Lógicos e Matemáticos em C++

--- operadoresMatematicos.cpp
+++ operadoresMatematicos.cpp
@@ -3,16 +3,23 @@ int main() {
 // Definindo os números para as operações
 int num1 = 15;
 int num2 = 20;
+int num3 = 7;
+int num4 = 15;
+int num5 = 5;
+int num6 = 6;
+int num7 = 20;
+int num8 = 4;
+
 // Realizando as operações matemáticas
 int soma = num1 + num2;
-int subtracao = num1 - num2;
-int multiplicacao = num1 * num2;
-int divisao = num1 / num2;
-int resto = num1 % num2; // Resto da divisão
+int subtracao = num3 - num4;
+int multiplicacao = num5 * num6;
+int divisao = num7 / num8;
+int resto = num7 % num8; // Resto da divisão
 // Imprimindo os resultados
 printf("Soma: %d + %d = %d\n", num1, num2, soma);
-printf("Subtração: %d - %d = %d\n", num1, num2, subtracao);
-printf("Multiplicação: %d * %d = %d\n", num1, num2, multiplicacao);
-printf("Divisão: %d / %d = %d (Quociente), Resto: %d\n", num1, num2, divisao, resto);
+printf("Subtração: %d - %d = %d\n", num3, num4, subtracao);
+printf("Multiplicação: %d * %d = %d\n", num5, num6, multiplicacao);
+printf("Divisão: %d / %d = %d (Quociente), Resto: %d\n", num7, num8, divisao, resto);
 return 0;
 }
